e5c3cf385c6b995c72ea496f1ab527e723d77061
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-05-23 15:36:27 +0600

    Backdoor.py

---
+++ Backdoor.py
@@ -0,0 +1,46 @@
+import os, random
+os.system("Clear")
+ban = random.randit(0,2)
+if ban == 0:
+  banner = '''
+    ______            _       _                   
+    | ___ \          | |     | |                  
+    | |_/ / __ _  ___| | ____| | ___   ___  _ __  
+    | ___ \/ _` |/ __| |/ / _` |/ _ \ / _ \| '__| 
+    | |_/ / (_| | (__|   < (_| | (_) | (_) | |    
+    \____/ \__,_|\___|_|\_\__,_|\___/ \___/|_|  '''  
+elif ban == 1:
+   banner = '''                                                                                                    
+  88888888ba                           88                 88                                           
+  88      "8b                          88                 88                                           
+  88      ,8P                          88                 88                                           
+  88aaaaaa8P'  ,adPPYYba,   ,adPPYba,  88   ,d8   ,adPPYb,88   ,adPPYba,    ,adPPYba,   8b,dPPYba,     
+  88""""""8b,  ""     `Y8  a8"     ""  88 ,a8"   a8"    `Y88  a8"     "8a  a8"     "8a  88P'   "Y8     
+  88      `8b  ,adPPPPP88  8b          8888[     8b       88  8b       d8  8b       d8  88             
+  88      a8P  88,    ,88  "8a,   ,aa  88`"Yba,  "8a,   ,d88  "8a,   ,a8"  "8a,   ,a8"  88             
+  88888888P"   `"8bbdP"Y8   `"Ybbd8"'  88   `Y8a  `"8bbdP"Y8   `"YbbdP"'    `"YbbdP"'   88         '''   
+ else:
+    banner = '''
+              ``             /\               /.
+              :d.           :Nm.            .s+       
+              /MN-         .m:/N-          `mmN       
+              /N:N:       .m+  :N-         hs/N       
+              /N -N/     `do    -N:       hh /N       
+              /N  .m+    hy      -N:     sh` /N       
+              /N   .mo  sh`       -m+   od`  /N       
+              /N    `dsom`         .m+ +m.   /N       
+              :MyyyyyhMMdyyyyyyyyyyydMhMdyyyydm       
+                     :Nhh`           :My              
+                    -N: sd`         -N/hy             
+                   .m+   om`       .m+  hy`           
+                  `do     +m.     `do    yh`          
+                  hy       /m.    dy      sh`         
+                 yh         /N:  hy        om`        
+                om`          -N/yh`         om.       
+               /m.            -md`           +m.      
+              ''`                             ::           
+ print(banner)                                                    '''
+     
+
+                                                                                                     
+                                                                                                     
