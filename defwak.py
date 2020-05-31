4c59beab2892ca2496b874c42571cd62d056b0bd
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-05-31 15:54:47 +0600

    Update defwak.py

--- defwak.py
+++ defwak.py
@@ -51,7 +51,8 @@ else:
 
 
 print(banner)
-text ='''          -{Main Manu}-
+text ='''          
+	  -{Main Manu}-
          -{Главное Меню}-
       Что вам будет угодно?(what do you want?)
         [1]-DSpamers
@@ -69,54 +70,8 @@ def DefSec0():
 	print("Прощайте!(Goodbye!)")
 	raise SystemExit
 def DefSec1():
-    os.system("сlear")
-    baner = '''
-	 _____                                     
-	/  ___|                                    
-	\ `--. _ __   __ _ _ __ ___   ___ _ __ ___ 
-	 `--. \ '_ \ / _` | '_ ` _ \ / _ \ '__/ __|
-	/\__/ / |_) | (_| | | | | | |  __/ |  \__ \
-	\____/| .__/ \__,_|_| |_| |_|\___|_|  |___/
-	      | |                                  
-	      |_|                                 '''
-
-    print(baner)
-    text = '''	  
-		  -{Menu}-
-		 --{Меню}--
-	      [1]-DSpamer(Version 1)
-	      [2]-DSpamer(Version 2)
-	      [3]-DSpamer(Version 3)
-
-	      [99]-Назад в главное меню(back to main menu)
-	      [0]-Выйти из программы(Exit the program)
-	      '''
-    print(text)
-    sp =int(input("Spamers~#:"))   
-    def DSV1():
-	os.system("python DSpamer.py")
-    def DSV2():
-	os.system("python DSpamerV2.py")
-    def DSV3():
-	os.system('python DSpamerV3open.py')
-    def DS99(): 
-	os.system('python defwak.py')
-    def DS0():
-	print('Прощайте!(Goodbye!)')
-	raise SystemExit 
-
-    if sp ==1:
-	DSV1()
-    elif sp ==2:
-	DSV2()
-    elif sp ==3:
-	DSV3()
-    elif sp ==0:
-	DS0()
-    elif sp ==99:
-	DS99()
-    else:
-	print('Ошибка:не найдено(Error:not found)')
+    os.system("python Spamers.py")
+ 
 def DefSec2():
   	os.system('clear')
 
