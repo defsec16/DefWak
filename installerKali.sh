105a31e6a2c108ceda028ee6ea8fe8b113713917
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-05 06:40:07 +0600

    Update installerKali.sh

--- installerKali.sh
+++ installerKali.sh
@@ -1,17 +1,21 @@
-echo "======Начинается установка:python,git,php,wget======"
+echo "======Начинается установка:python,git,php,wget,scapy======"
 
-sudo apt-get install python -y
+apt-get install python3 -y
 
 echo "======Установлен Python======"
 
-sudo apt-get install git -y 
+apt-get install git -y 
 
 echo "======Установлен Git======"
 
-sudo apt-get install wget -y
+apt-get install wget -y
 
 echo "======Установлен Wget======"
 
-sudo apt-get install php -y
+apt-get install php -y
 
 echo "======Установлен PHP======"
+
+apt-get install scapy-python3
+
+echo "======Установлен Scapy======"
