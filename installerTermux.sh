f8dda6fe02f3d8a269cedf11fcfede70d9f2b3cf
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-05 19:50:51 +0600

    Update installerTermux.sh

--- installerTermux.sh
+++ installerTermux.sh
@@ -1,13 +1,13 @@
-echo "======Начинается установка:python,php,wget======"
+echo -e "\e[31m======Начинается установка:python,php,wget======\e[0m"
 
 pkg install python -y
 
-echo "======Установлен Python======"
+echo -e "\e[31m======Установлен Python======\e[0m"
 
 pkg install wget -y
 
-echo "======Установлен Wget======"
+echo -e "\e[31m======Установлен Wget======\e[0m"
 
 pkg install php -y
 
-echo "======Установлен PHP======"
+echo -e "\e[31m======Установлен PHP======\e[0m"
