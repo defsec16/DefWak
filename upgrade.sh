07bd1eafec955c3eb88b81b8bbed360a822e2ee4
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-05 19:45:14 +0600

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,6 @@
 #обновление до последней версии
 cd
-git clone --depth=1 https://github.com/defsec16/defwak.git
+git clone --depth=1 https://github.com/defsec16/DefWak.git
+cd DefWak
 chmod +x *
 echo "всё готово"
