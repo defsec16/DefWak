d7b68cffb4a3af343f82689e0ceb8d684a31e9b2
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-05 06:36:34 +0600

    Create installerKali.sh

---
+++ installerKali.sh
@@ -0,0 +1,17 @@
+echo "======Начинается установка:python,git,php,wget======"
+
+sudo apt-get install python -y
+
+echo "======Установлен Python======"
+
+sudo apt-get install git -y 
+
+echo "======Установлен Git======"
+
+sudo apt-get install wget -y
+
+echo "======Установлен Wget======"
+
+sudo apt-get install php -y
+
+echo "======Установлен PHP======"
