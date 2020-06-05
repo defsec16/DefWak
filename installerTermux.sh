30a04e3c26052f762b97437c51f1718197022bd9
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-05 06:33:28 +0600

    Create installerTermux.sh

---
+++ installerTermux.sh
@@ -0,0 +1,17 @@
+echo "======Начинается установка:python,git,php,wget======"
+
+pkg install python -y
+
+echo "======Установлен Python======"
+
+pkg install git -y 
+
+echo "======Установлен Git======"
+
+pkg install wget -y
+
+echo "======Установлен Wget======"
+
+pkg install php -y
+
+echo "======Установлен PHP======"
