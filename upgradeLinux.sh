4e2b4d3b1a584cd0ebe2f975c5ff703f78513916
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-07 17:17:25 +0600

    Update upgradeLinux.sh

--- upgradeLinux.sh
+++ upgradeLinux.sh
@@ -1,5 +1,12 @@
+
+cd
+
 cd
 git clone --depth=1 https://github.com/defsec16/DefWak.git
 cd
 sudo chmod +x *
-echo "всё готово!"
+
+clear
+
+echo -e "\e[32m[✔] Отлично готово! (all good!)[✔]\e[0m"
+
