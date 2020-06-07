e2d273172313ba6fdd841eb53e07cc28db95bc5d
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-07 17:22:18 +0600

    Update upgradeLinux.sh

--- upgradeLinux.sh
+++ upgradeLinux.sh
@@ -45,5 +45,5 @@ sudo chmod +x *
 clear
 
 echo -e "\e[32m[✔] Отлично готово! (all good!)[✔]\e[0m"
-echo -e "\e[34mВпишите команду <cd>(Enter the <cd> command)\e[0"
+echo -e "\e[33mВпишите команду <cd>(Enter the <cd> command)\e[0"
 
