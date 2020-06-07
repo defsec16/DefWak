639f91b7d54ce7ea7d77c119fb9d2875f02c6c08
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-07 17:20:26 +0600

    Update upgradeLinux.sh

--- upgradeLinux.sh
+++ upgradeLinux.sh
@@ -1,3 +1,39 @@
+#обновление до последней версии
+INSTALL_DIR="$HOME/DefWak"
+
+BIN_DIR="/usr/local/bin/"
+
+BASH_PATH="/bin/bash"
+
+TERMUX=false
+
+
+echo "\033[32m[✔] Checking directories...(Проверка каталогов...)";
+
+if [ -d "$INSTALL_DIR" ]; then
+
+        rm -rf "$INSTALL_DIR"
+
+        rm "$BIN_DIR/DefWak*"
+
+        sudo rm -rf "$INSTALL_DIR"
+
+        sudo rm "$BIN_DIR/DefWak*"
+
+    else
+
+        echo "[✘] If you want to uninstall you must remove previous installations ";
+
+        echo "[✘] Если вы хотите удалить, вы должны удалить предыдущие установки "
+
+        echo "[✘] Провал! (Failed!)[✘]";
+
+fi
+
+
+clear
+
+echo -e "\e[32m[✔] Отлично готово! (all good!)[✔]\e[0m"
 
 cd
 
@@ -9,4 +45,5 @@ sudo chmod +x *
 clear
 
 echo -e "\e[32m[✔] Отлично готово! (all good!)[✔]\e[0m"
+echo -e "\e[34mВпишите команду <cd>(Enter the <cd> command)\e[0"
 
