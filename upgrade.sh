fba5760afc93ff02bec3860814a8456ca0d7d886
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-07 17:09:55 +0600

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,48 @@
 #обновление до последней версии
+INSTALL_DIR="$HOME/DefWak"
+
+BIN_DIR="/usr/local/bin/"
+
+BASH_PATH="/bin/bash"
+
+TERMUX=false
+
+
+echo "\033[32m[✔] Checking directories...(Проверка каталогов...)";
+
+if [ -d "$INSTALL_DIR" ]; then
+
+        rm -rf "$INSTALL_DIR"
+
+        rm "$BIN_DIR/DefWak*"
+
+        sudo rm -rf "$INSTALL_DIR"
+
+        sudo rm "$BIN_DIR/DefWak*"
+
+    else
+
+        echo "[✘] If you want to uninstall you must remove previous installations ";
+
+        echo "[✘] Если вы хотите удалить, вы должны удалить предыдущие установки "
+
+        echo "[✘] Провал! (Failed!)[✘]";
+
+fi
+
+
+clear
+
+echo -e "\e[32m[✔] Отлично готово! (all good!)[✔]\e[0m"
+
+cd
+
 cd
 git clone --depth=1 https://github.com/defsec16/DefWak.git
 cd DefWak
 chmod +x *
-echo "всё готово"
+
+clear
+
+echo -e "\e[32m[✔] Отлично готово! (all good!)[✔]\e[0m"
+
