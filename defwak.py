a8d6e0f1b3ff199e70a66de5b163d5f74ae37736
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-09 16:48:47 +0600

    Update defwak.py

--- defwak.py
+++ defwak.py
@@ -6,8 +6,25 @@ def clean():
 		os.system('clear')
 	except:
 		os.system('cls')
-clean()
+def play():
+	clean()
+	os.chdir('gif')
+	gif = ["we.txt","wd.txt","wa.txt","ws.txt","gf.txt","wq.txt","wg.txt","wc.txt","wf.txt","wr.txt","wx.txt","wz.txt","gh.txt","gd.txt"]
+	def animator(gif,delay=1,repeat=10000):
+		frames = []
+		for wak in gif:
+			with open(wak,"r",encoding="utf-8") as d:
+				frames.append(d.readlines())
+		for i in range(repeat):
+			for frame in frames:
+				print("".join(frame))
+				time.sleep(delay)
+				clean()
+	animator(gif,delay=0.5,repeat=5)
 
+clean()
+play()
+clean()
 
 ban = random.randint(0,3)
 if ban ==0:
