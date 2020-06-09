6f701d056c7ef8b0d2e379864d569ad678f0a391
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-10 02:26:20 +0600

    Update Spamers.py

--- Spamers.py
+++ Spamers.py
@@ -24,6 +24,7 @@ text = '''	\033[35m
       [1]-DSpamer(Version 1)
       [2]-DSpamer(Version 2)
       [3]-DSpamer(Version 3)
+      [4]-Mail spam
       \033[36m
       [99]-Назад в главное меню(back to main menu)
       [0]-Выйти из программы(Exit the program)
@@ -36,6 +37,8 @@ def DSV2():
   os.system("python DSpamerV2.py")
 def DSV3():
   os.system('python DSpamerV3open.py')
+def mail():
+  os.system('python spammail.py')
 def DS99(): 
   os.system('python defwak.py')
 def DS0():
@@ -47,6 +50,8 @@ elif sp == 2:
   DSV2()
 elif sp == 3:
   DSV3()
+elif sp ==4:
+  mail()
 elif sp == 0:
   DS0()
 elif sp ==99:
