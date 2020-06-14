ef5619f98bf66c7486aa15c12947ded1737344d4
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-15 05:00:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -43,5 +43,40 @@ banner = """
 print(banner)
 sleep(4)
 print('\033[32m')
+banner2 = """                                                                                                             
+                                                                 
+                                      \033[32m                                          
+                                     .,,,,,,,,,,,.                                            
+                                  .',:odddodddoolc;'.                                         
+                               .'cllllloollodoooooolc:;.                                      
+                             .':llllclc:clcloolccloolcc:'.                                    
+                            ';:llc::ccc::c:collc:cl:;;:cc:'.                                  
+                           ;lccclc;;;;;::c::cccclllcclolool,.                                 
+                          'looc;,'....'cc;..';cll;'''';coool,                                 
+                         .:lol,.      .;c.   .c;..     .'ldo:.                                
+                         .:c:'         .:'   .;,         'llc;.                               
+                         ,::;.      ...;'     .c;.       .:cl;.                               
+                        .':c:.   ..';;,'.      ;ol;'.''. .:loc.                               
+                        .,;:c;,,;:llc:;.. .... ,looolooc::cccc,                               
+                       .'::::cc::ccc:cc:;;ccc::clooooodoooc::c:.                              
+                       .';;,',,.':cccccccccclllllooodl;,;;,',lc.                              
+                        .;;,..   .;llc::c:cllllllllo:.    .,cl;.                              
+                        .;;:c:,.  .cc:,;c::clccccool'  .;:;cc:'                               
+                        ..,;cc;.  ;oc;,;:;;cl:;:lolo:...cccc;.                                
+                          .;:ccc,.coc:'.....'...'lolc..;c;;c;.                                
+                          .',::c;';c;..          .'::'.:l:::,.                                
+                            .;c:. ..               .. .co:...                                 
+                             ,c;.      Def-Wak         .co:.                                   
+                            '::;'.                   .;ool,.                                  
+                            .;:::,                   ,lool,                                                                  
+                             .;loool;.           .,cllll:.                                    
+                               ,llloc;,..',,''...,c::cc,.                                     
+                                .:clllllllllollcclllc;'                                       
+                                 .'coooolclc:cllcoo:..                                        
+                                   .;lollool:cllloc'                                          
+                                     .:oooooooll;.                                            
+                                      ..........          
+                                   """        
+print(banner2)
 os.system('python zagruzka.py')
 os.system('python defwak.py')
